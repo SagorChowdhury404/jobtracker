d2c1f13bfd5a71a7cfc6068b819f333b3fadc8cd
sagorchowdhury <rajchowdhury820@gmail.com>
2026-03-04 02:59:30 +0400

    added toggleStyle

--- script/tracker.js
+++ script/tracker.js
@@ -1,8 +1,9 @@
 
-
+const totalInterviewList = [{ "name": "sagy" }];
+const totalRejectedList = [1, 2, 3,];
 
 let totalJOb = document.getElementById("totalJobCount");
-let totalInterviewCount = document.getElementById("totalJobCount");
+let totalInterviewCount = document.getElementById("totalInterviewCount");
 let totalRejectedCount = document.getElementById("totalRejectedCount");
 
 
@@ -10,10 +11,37 @@ let totalRejectedCount = document.getElementById("totalRejectedCount");
 const allCards = document.getElementById("allCards");
 
 
-
-
 function totalCalculatorCount() {
-    totalJOb.innerText = allCards.children.length
+    totalJOb.innerText = allCards.children.length;
+    totalInterviewCount.innerText = totalInterviewList.length;
+    totalRejectedCount.innerText = totalRejectedList.length;
 
 }
 totalCalculatorCount();
+// all-filter-btn applied-filter-btn rejects-filter-btn
+const allFilterBtn = document.getElementById('all-filter-btn');
+const appliedFilterBtn = document.getElementById('applied-filter-btn');
+const rejectsFilterBtn = document.getElementById('rejects-filter-btn');
+
+function toggleStyle(id) {
+
+    // Step 1: Make all buttons default
+    allFilterBtn.classList.remove('bg-blue-600', 'text-white');
+    appliedFilterBtn.classList.remove('bg-blue-600', 'text-white');
+    rejectsFilterBtn.classList.remove('bg-blue-600', 'text-white');
+
+    allFilterBtn.classList.add('bg-slate-200', 'text-black');
+    appliedFilterBtn.classList.add('bg-slate-200', 'text-black');
+    rejectsFilterBtn.classList.add('bg-slate-200', 'text-black');
+
+    // Step 2: Select clicked button
+    const selected = document.getElementById(id);
+
+    selected.classList.remove('bg-slate-200', 'text-black');
+    selected.classList.add('bg-blue-600', 'text-white');
+}
+
+
+
+
+
