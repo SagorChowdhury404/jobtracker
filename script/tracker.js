d6e20c54975ec9e44b1aeb8772b450c57f26d136
sagorchowdhury <rajchowdhury820@gmail.com>
2026-03-03 18:16:51 +0400

    added totalJOb count

--- script/tracker.js
+++ script/tracker.js
@@ -1,1 +1,19 @@
-console.log('tracker')
+
+
+
+let totalJOb = document.getElementById("totalJobCount");
+let totalInterviewCount = document.getElementById("totalJobCount");
+let totalRejectedCount = document.getElementById("totalRejectedCount");
+
+
+// totalJOb --allCards 
+const allCards = document.getElementById("allCards");
+
+
+
+
+function totalCalculatorCount() {
+    totalJOb.innerText = allCards.children.length
+
+}
+totalCalculatorCount();
